42d14e923f5e2dcdd285ac7a2e84be063aefa184
gerardo <gerardo@recemed.com>
2025-10-30 14:49:38 -0300

    refactor: clean up AddMemberModal component by removing unused variables and functions

--- src/app/components/admin/MemberManagement/AddMemberModal.vue
+++ src/app/components/admin/MemberManagement/AddMemberModal.vue
@@ -1,43 +1,14 @@
 <style scoped lang="scss" src="./AddMemberModal.scss" />
 <script setup lang="ts">
-import { ref, computed } from 'vue';
+import { computed, ref } from 'vue';
 import { useStore } from '../../../../middlewares/store';
-import { classes } from '../../../../middlewares/misc/const';
 import CustomModal from '../../Modals/CustomModal.vue';
 import LabeledInput from '../../Inputs/LabeledInput.vue';
 
 const emit = defineEmits(['close']);
 const store: any = useStore();
-
-const whatsapp = ref('');
 const name = ref('');
-const resonance = ref(0);
-const selectedClass = ref('');
-const sliderContainer = ref<HTMLElement | null>(null);
-
-function scrollLeft() {
-  if (sliderContainer.value) {
-    sliderContainer.value.scrollBy({
-      left: -200, // Scroll by 200px to the left
-      behavior: 'smooth'
-    });
-  }
-}
-
-function scrollRight() {
-  if (sliderContainer.value) {
-    sliderContainer.value.scrollBy({
-      left: 200, // Scroll by 200px to the right
-      behavior: 'smooth'
-    });
-  }
-}
-
 const isCharacterEmpty = computed(() => name.value.trim() === '');
-
-function handleClassSelection(classValue: string) {
-  selectedClass.value = classValue;
-};
 
 function handleCloseModal() {
   emit('close');
